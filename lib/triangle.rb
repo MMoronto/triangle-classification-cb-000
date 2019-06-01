e7f8f316e9d9fda642d74a3f17cdd40653529344
MOBOLA MORONTO <t2wycemo@gmail.com>
2019-06-01 03:01:01 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,32 @@
 class Triangle
-  # write code here
+    attr_accessor :length_a, :length_b, :length_c
+  
+    def initialize(length_a, length_b, length_c)
+      @length_a = length_a 
+      @length_b = length_b
+      @length_c = length_c  
+    end 
+  
+    def kind
+      validate_triangle
+      if length_a == length_b && length_b == length_c
+          :equilateral
+      elsif length_a == length_b || length_b == length_c || length_a == length_c
+          :isosceles
+      else
+          :scalene
+      end 
+    end
+    
+    def validate_triangle
+      legit_triangle = [(length_a + length_b > length_c),
+      (length_a + length_c > length_b), (length_b + length_c > length_a)]
+      [length_a, length_b, length_c].each {|side| legit_triangle << false if side <= 0}
+      raise TriangleError if legit_triangle.include?(false)
+    end
+    
+    class TriangleError < StandardError
+      # triangle error code
+    end
+    
 end
