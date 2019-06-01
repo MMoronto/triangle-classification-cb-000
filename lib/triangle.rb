94ea3af64e60af0896d80b5a2775f9165a7a3ba2
MOBOLA MORONTO <t2wycemo@gmail.com>
2019-06-01 02:53:09 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,6 +8,7 @@ class Triangle
     end 
   
     def kind
+      validate_triangle
       if length_a == length_b && length_b == length_c
           :equilateral
       elsif length_a == length_b || length_b == length_c || length_a == length_c
