1400e7c0644edf77ea97143d1e8bad8895e6050e
MOBOLA MORONTO <t2wycemo@gmail.com>
2019-06-01 02:19:11 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,16 @@
 class Triangle
+  attr_accessor :length, :equilateral, :isoceles, :scalene
+  
+  def initialize(length, length, length)
+    @length = length 
+    @equilateral = equilateral
+    @isoceles = isoceles
+    @scalene = scalene
+  end 
+  
+  def kind(triangle) 
+    self.equilateral = triangle
+    if triangle(2, 4, 2)
+  end 
   # write code here
 end
